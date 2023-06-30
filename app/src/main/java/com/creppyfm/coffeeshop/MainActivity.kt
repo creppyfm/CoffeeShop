1c36a5154528a7441e9b2343294220a3c191d9c9
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-06-29 22:08:09 -0400

    Added mutableStateOf example.

--- app/src/main/java/com/creppyfm/coffeeshop/MainActivity.kt
+++ app/src/main/java/com/creppyfm/coffeeshop/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,12 +56,27 @@ fun GreetingPreview() {
     }
 }*/
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun ComposableExample() {
-    Text("Hello, World!",
-        modifier = Modifier
-            .padding(16.dp)
-            .background(Color.Green)
-            .padding(16.dp))
+    var name = remember { mutableStateOf("") }
+
+    Column() {
+        Text("Hello, ${name.value}!",
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.Green)
+                .padding(16.dp))
+
+        Text("Enter name below:",
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.Green)
+                .padding(4.dp))
+
+        TextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+    }
 }
