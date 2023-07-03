35bbbcaf1129f69086580dc0b00ba20f0d46e54b
creppyfm <97473588+creppyfm@users.noreply.github.com>
2023-07-03 18:32:55 -0400

    Added top app bar and bottom navigation bar.

--- app/src/main/java/com/creppyfm/coffeeshop/App.kt
+++ app/src/main/java/com/creppyfm/coffeeshop/App.kt
@@ -1,24 +1,43 @@
 package com.creppyfm.coffeeshop
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.creppyfm.coffeeshop.ui.theme.Alternative2
+import com.creppyfm.coffeeshop.ui.theme.CoffeeShopTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
+fun App_Preview() {
+    CoffeeShopTheme {
+        App()
+    }
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun App() {
+    var selectedRoute = remember {
+        mutableStateOf(Routes.MenuPage.route)
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -28,20 +47,24 @@ fun App() {
             )
         },
         bottomBar = {
-            BottomAppBar {}
+            NavBar(
+                selectedRoute = selectedRoute.value,
+                onChange = {newRoute ->
+                selectedRoute.value = newRoute
+            })
         }
-    ) { paddingValues ->
-        Text(text = "This is the Application content.",
-            modifier = Modifier.padding(paddingValues))
+    ) {
+        OffersPage()
     }
 }
-
-@Preview
 @Composable
 fun AppTitle() {
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Alternative2)
+            .padding(8.dp)
     ) {
         Image(painter = painterResource(id = R.drawable.logo),
             contentDescription = "Coffee Masters Logo")
---
+++ app/src/main/java/com/creppyfm/coffeeshop/BottomNavigation.kt
@@ -0,0 +1,92 @@
+package com.creppyfm.coffeeshop
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.creppyfm.coffeeshop.ui.theme.Alternative1
+import com.creppyfm.coffeeshop.ui.theme.Alternative2
+import com.creppyfm.coffeeshop.ui.theme.OnPrimary
+
+data class NavigationPage(var name: String, var icon: ImageVector, var route: String)
+
+object Routes {
+    val MenuPage = NavigationPage("Menu", Icons.Outlined.Menu, "menu")
+    val OffersPage = NavigationPage("Offers", Icons.Outlined.Star, "offers")
+    val OrderPage = NavigationPage("My Order", Icons.Outlined.ShoppingCart, "order")
+    val InfoPage = NavigationPage("Info", Icons.Outlined.Info, "info")
+
+    val pages = listOf(MenuPage, OffersPage, OrderPage, InfoPage)
+}
+
+@Preview
+@Composable
+fun NavBarPreview() {
+
+}
+
+
+@Composable
+fun NavBar(selectedRoute: String = Routes.MenuPage.route,
+           onChange: (String) -> Unit
+           ) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Alternative2)
+            .padding(8.dp)
+    ) {
+        for (page in Routes.pages) {
+            NavBarItem(
+                page,
+                selected = selectedRoute == page.route,
+                Modifier.clickable {
+                    onChange(page.route)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun NavBarItem(page: NavigationPage, selected: Boolean = false, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(horizontal = 12.dp)) {
+        Image(
+            imageVector = page.icon,
+            contentDescription = page.name,
+            colorFilter = ColorFilter.tint(
+                if (selected) Alternative1 else OnPrimary
+            ),
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .size(24.dp)
+        )
+        Text(page.name,
+            fontSize = 12.sp,
+            color = if (selected) Alternative1 else OnPrimary
+        )
+    }
+}
+
